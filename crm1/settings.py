f542a36daadef9f9507ad98d369a561fea6e9260
eric693 <75469714+eric693@users.noreply.github.com>
2022-06-16 13:12:18 +0800

    Add files via upload

--- crm1/settings.py
+++ crm1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['eric-project.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -131,8 +131,11 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
